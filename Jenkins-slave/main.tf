916738935d5506a5561e2b0cba06c34f66792d20
Velocity9919 <143435067+Velocity9919@users.noreply.github.com>
2024-02-12 20:57:48 +0530

    Update main.tf

--- Jenkins-slave/main.tf
+++ Jenkins-slave/main.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "Jenkins-master" {
+resource "aws_instance" "Jenkins-slave" {
   ami                    = var.ami_id
   instance_type          = var.my_instance_type
   key_name               = var.my_key
   vpc_security_group_ids = [aws_security_group.web-traffic.id]
 
   tags = {
-    "Name" = "Jenkins-master"
+    "Name" = "Jenkins-slave"
   }
 }
