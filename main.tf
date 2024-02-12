ef1bc4a9f380bf986014bdef9da06d2853a43800
Velocity9919 <143435067+Velocity9919@users.noreply.github.com>
2024-02-12 20:58:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "Jenkins-master" {
+resource "aws_instance" "SonarqubeServer" {
   ami                    = var.ami_id
   instance_type          = var.my_instance_type
   key_name               = var.my_key
   vpc_security_group_ids = [aws_security_group.web-traffic.id]
 
   tags = {
-    "Name" = "Jenkins-master"
+    "Name" = "Sonarqube-server"
   }
 }
